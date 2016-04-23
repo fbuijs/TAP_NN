7d649ac708d78823a6ee31e05ac0557dc208d45d
franck <fbuijs45@gmail.com>
2016-04-23 11:49:28 +0200

    create setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TAP_NN',
+    version='0.1.0',
+    packages=['pybrain', 'matplotlib', 'unicodedata'],
+    url='',
+    license='',
+    author='Franck Buijs',
+    author_email='',
+    description='Speech processing with neural network'
+)
